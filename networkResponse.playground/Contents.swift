2720f9d22501def2a0ebaeb5e97443333e78df15
Anton Yatsenko <yacenko372o@gmail.com>
2022-10-27 20:50:15 +0300

    first commit

--- networkResponse.playground/Contents.swift
+++ networkResponse.playground/Contents.swift
@@ -6,7 +6,7 @@ let statusCode = Int.random(in: 100...602)
 let informational = responseCode("Information", statusCode, 100, 199, "Request received, continuing process")
 let successful = responseCode("Successful", statusCode, 200, 299, "The action was successfuly received, understood, and accepted")
 let redirection = responseCode("Redirection", statusCode, 300, 399, "Futher action must be taken in order to complete the request")
-let clientError = responseCode("Client Error", statusCode, 400, 499, "The request contains bad syntax or cannot be fulfiled")
+let clientError = responseCode("Client Error", statusCode, 400, 499, "The request contains bad syntax or can not be fulfiled")
 let serverError = responseCode("Server Error", statusCode, 500, 599, "The server failed to fulfill an apparently valid request")
 
 switch statusCode{
